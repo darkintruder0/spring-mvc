b2c566b2025c53d68de53e6424b45410faafe4f4
Efrain Mendoza <efrain.mendoza@amucss.org>
2019-01-12 09:07:59 -0600

    test3

--- spring-mvc/src/main/java/net/tecgurus/spring/mvc/controller/CatalogoController.java
+++ spring-mvc/src/main/java/net/tecgurus/spring/mvc/controller/CatalogoController.java
@@ -1,5 +1,9 @@
 package net.tecgurus.spring.mvc.controller;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +11,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import net.tecgurus.spring.mvc.beans.Catalogo;
+import net.tecgurus.spring.mvc.entities.CatalogoEntity;
+import net.tecgurus.spring.mvc.repository.CatalogoRepository;
 
 @Controller
 public class CatalogoController {
+	
+	@Autowired  // Inyeccion de dependencia
+	private CatalogoRepository catalogoRepository;
 	
 	@GetMapping("/catalogo")
 	public String GetFormCatalogo(Model model) {
@@ -24,6 +33,17 @@ public class CatalogoController {
 		System.out.println("u: "+catRequest.getDesCorCat());
 		System.out.println("u: "+catRequest.getDesComCat());
 		System.out.println("u: "+catRequest.getIdEstCat());
+		
+		// Obj Catalogo Entity
+		CatalogoEntity catalogoEntity = new CatalogoEntity();
+		catalogoEntity.setDsc_cor_cat(catRequest.getDesCorCat());
+		catalogoEntity.setDsc_com_cat(catRequest.getDesComCat());
+		catalogoEntity.setId_est_cat(catRequest.getIdEstCat());
+		
+		catalogoEntity.setFch_cre(new Timestamp(new Date().getTime()));
+		catalogoEntity.setFch_mod(new Timestamp(new Date().getTime()));
+		
+		catalogoRepository.save(catalogoEntity);
 		
 		return "redirect:/catalogo";
 	}
--- spring-mvc/src/main/java/net/tecgurus/spring/mvc/controller/LoginController.java
+++ spring-mvc/src/main/java/net/tecgurus/spring/mvc/controller/LoginController.java
@@ -3,6 +3,9 @@
  */
 package net.tecgurus.spring.mvc.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import net.tecgurus.spring.mvc.beans.Login;
+import net.tecgurus.spring.mvc.entities.LoginEntity;
+import net.tecgurus.spring.mvc.repository.LoginRepository;
 
 /**
  * @author Allan Flores Rojas
@@ -17,6 +22,9 @@ import net.tecgurus.spring.mvc.beans.Login;
  */
 @Controller
 public class LoginController {
+	
+	@Autowired  // Inyeccion de dependencia
+	private LoginRepository loginRepository; 
 	
 	@GetMapping("/login")
 	public String getLogin(Model model) {
@@ -32,7 +40,46 @@ public class LoginController {
 		System.out.println("u: "+loginRequest.getEmail());
 		System.out.println("u: "+loginRequest.getPassword());
 		
-		return "redirect:/catalogo";
+		// Obj Login Entity
+		LoginEntity loginEntity = new LoginEntity();
+		loginEntity.setEmail(loginRequest.getEmail());
+		loginEntity.setPassword(loginRequest.getPassword());
+		
+		LoginEntity loginRepo = loginRepository.findByEmailAndPassword(
+				loginEntity.getEmail(), 
+				loginEntity.getPassword() );
+		
+		loginRepository.save(loginEntity);
+		
+		if (loginRepo == null) {
+			System.out.println("No existe");
+			return "redirect:/login";
+		} else {
+			System.out.println("ID user: " +loginRepo.getId_login() );
+			return "redirect:/catalogo"; 
+		}
+		
 	}
-
+	
+	@GetMapping("/usuarios")
+	public String getUsuarios(Model model)
+	{
+		
+		List<LoginEntity> lista_usuarios = (List<LoginEntity>)loginRepository.findAll(); // (casteo) select *
+		
+		// Información
+		for (LoginEntity user : lista_usuarios) {
+			System.out.println("u: " +user.getEmail());
+			System.out.println("u: " +user.getPassword());
+			System.out.println("u: " +user.getId_login());
+		}
+		
+		// Se guada en memoria
+		model.addAttribute("lista_usuarios", lista_usuarios );
+		
+		return "usuarios";
+		// return "redirect:/usuarios.html"; se manda a llamar a si mismo infinitamente 
+		
+	}
+	
 }
